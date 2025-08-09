c3315243057d81e4c1c6c9eff448a1ef7463aa2c
One Prism <tiktoknovocell40@gmail.com>
2025-08-09 17:30:44 -0300

    Update questionSpoof.js

--- functions/questionSpoof.js
+++ functions/questionSpoof.js
@@ -1,31 +1,54 @@
-const phrases = [ 
-    "KhanTool: https://discord.gg/PYNQfcDvZy",
-    "Soluções blue sem enrolação.",
-    "Estudo premiado com KhanTool.",
-    "Acesse o painel KhanTool.",
-    "Vem pra KhanTool.",
-];
-const originalFetch_qs = window.fetch;
+const originalFetch = window.fetch;
+
 window.fetch = async function (input, init) {
     let body;
     if (input instanceof Request) body = await input.clone().text();
     else if (init && init.body) body = init.body;
-    const originalResponse = await originalFetch_qs.apply(this, arguments);
+
+    const originalResponse = await originalFetch.apply(this, arguments);
     const clonedResponse = originalResponse.clone();
+
     try {
         const responseBody = await clonedResponse.text();
         let responseObj = JSON.parse(responseBody);
-        if (features.questionSpoof && responseObj?.data?.assessmentItem?.item?.itemData) {
+
+        if (responseObj?.data?.assessmentItem?.item?.itemData) {
             let itemData = JSON.parse(responseObj.data.assessmentItem.item.itemData);
-            if(itemData.question.content[0] === itemData.question.content[0].toUpperCase()){
-                itemData.answerArea = { "calculator": false, "chi2Table": false, "periodicTable": false, "tTable": false, "zTable": false }
-                itemData.question.content = phrases[Math.floor(Math.random() * phrases.length)] + `[[☃ radio 1]]`;
-                itemData.question.widgets = { "radio 1": { type: "radio",  options: { choices: [ { content: "Resposta correta.", correct: true }, { content: "Resposta incorreta.", correct: false } ] } } };
+
+            if (itemData.question.content[0] === itemData.question.content[0].toUpperCase()) {
+                itemData.answerArea = { 
+                    "calculator": false, 
+                    "chi2Table": false, 
+                    "periodicTable": false, 
+                    "tTable": false, 
+                    "zTable": false 
+                };
+
+                itemData.question.content = "🔧 KhanTool: https://discord.gg/PYNOfcDvZvile [[☃ radio 1]]";
+                
+                itemData.question.widgets = { 
+                    "radio 1": { 
+                        type: "radio", 
+                        options: { 
+                            choices: [ 
+                                { content: "Resposta correta.", correct: true }, 
+                                { content: "Resposta incorreta.", correct: false } 
+                            ] 
+                        } 
+                    } 
+                };
+
                 responseObj.data.assessmentItem.item.itemData = JSON.stringify(itemData);
-                sendToast("Questão completada!", 1200);
-                return new Response(JSON.stringify(responseObj), { status: originalResponse.status, statusText: originalResponse.statusText, headers: originalResponse.headers });
+                sendToast("🔓 Questão exploitada pelo KhanTool.", 1000);
+
+                return new Response(JSON.stringify(responseObj), { 
+                    status: originalResponse.status, 
+                    statusText: originalResponse.statusText, 
+                    headers: originalResponse.headers 
+                });
             }
         }
-    } catch (e) { debug(`Erro @ questionSpoof.js\n${e}`); }
+    } catch (e) { }
+
     return originalResponse;
 };
